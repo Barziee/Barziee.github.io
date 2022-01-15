1b4b68b9feb3b37af7888719939e9729fd4542ed
volking0 <volking0@gmail.com>
2022-01-15 22:04:13 +0200

    vue router - remove history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import AboutView from "../views/AboutView.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -10,25 +10,19 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: "/resume",
     name: "resume",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "resume" */ "@/views/ResumeView.vue"),
   },
   {
     path: "/projects",
     name: "projects",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "projects" */ "@/views/ProjectsView.vue"),
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
